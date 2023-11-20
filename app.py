656e45a57501f7aabfb6afc034209a8fd0c08c31
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-21 00:10:45 +0900

    데이터 크롤링한거 딕셔너리로 갖고오고 그리드박스까지 만들었음

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask, render_template
+import requests, math
+from bs4 import BeautifulSoup
 app = Flask(__name__)
+
+@app.route("/")
+def lolPsdata():
+    url="https://fow.kr/stats"
+    response = requests.get(url)
+
+    champData = {}
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for i, tr_tag in enumerate(soup.find_all('tr')):
+        position = tr_tag.get('position')
+        rname = tr_tag.get('rname')
+
+        champData.setdefault(i, [position, rname])
+
+    for i, rate in enumerate(soup.find_all('td', class_='td_rate')):
+        champData[math.trunc(i/6)+1].append(rate.text)
+
+    return render_template('index.html', champData=champData )
+
 
 @app.route('/',methods=['GET','POST']) 
 def hello_world():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
