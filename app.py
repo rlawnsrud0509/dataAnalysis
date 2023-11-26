2da08d8bb5c5e78fcb10a0b3a824f249f1f6c7e7
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-26 20:08:35 +0900

    보여주는거까지 성공 ~

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import requests, math
+import requests, json
 from bs4 import BeautifulSoup
 app = Flask(__name__)
 
@@ -9,19 +9,26 @@ def lolPsdata():
     response = requests.get(url)
 
     champData = []
+    array= []
+    rateArray = []
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    for i, tr_tag in enumerate(soup.find_all('tr')):
+
+    for i, rate in enumerate(soup.find_all('td', class_='td_rate'), start=0):
+        rateArray.append(rate.text)
+        if (i+1) % 6 == 0:
+            array.append(rateArray)
+            rateArray = []
+
+    for i, tr_tag in enumerate(soup.find_all('tr'), start=0):
         position = tr_tag.get('position')
         rname = tr_tag.get('rname')
 
-        champData.append({i: [rname, position]})
+        champData.append([rname, position, *array[i-1]])
 
-    for i, rate in enumerate(soup.find_all('td', class_='td_rate')):
-        champData[math.trunc(i/6)+1][i].append(rate)
+    champData_json = json.dumps(champData)
 
-    return render_template('index.html', champData=champData )
-
+    return render_template('index.html', champData=champData_json )
 
 @app.route('/',methods=['GET','POST']) 
 def hello_world():
